b39edeb566d4cfa2f6a4c433e691b7791d2c242e
Eche <echyth@yahoo.com>
2024-01-12 12:56:03 +1000

    fix: update message when pop fails

--- opcodes.c
+++ opcodes.c
@@ -36,7 +36,7 @@ void _pop(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%u: can't pop and empty stack\n", line_number);
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
 		free_resources();
 		exit(EXIT_FAILURE);
 	}
